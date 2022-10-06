71214328c8fbc186f472a4eaa9bf7b31871bb76a
Mahiwal Sahu <mmahiwal.dat20@gmail.com>
2022-10-06 13:05:58 +0530

    heroku deployement

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -133,6 +133,6 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT= os.path.join(BASE_DIR,'static'),
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 django_heroku.settings(locals())
